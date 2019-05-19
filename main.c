76acb8c056734dc068f68b403609edd030d69f16
easyaspi314 (Devin) <easyaspi314@users.noreply.github.com>
2019-05-18 23:05:35 -0400

    Add support for reading files.

--- main.c
+++ main.c
@@ -1,8 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include "brainfuck-jit.h"
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    const char test[] = ">++[<+++++++++++++>-]<[[>+>+<<-]>[<+>-]++++++++[>++++++++<-]>.[-]<<>++++++++++[>++++++++++[>++++++++++[>++++++++++[>++++++++++[>++++++++++[>++++++++++[-]<-]<-]<-]<-]<-]<-]<-]++++++++++.";
-    brainfuck(test, sizeof(test));
+    if (argc == 1) {
+        const char test[] = ">++[<+++++++++++++>-]<[[>+>+<<-]>[<+>-]++++++++[>++++++++<-]>.[-]<<>++++++++++[>++++++++++[>++++++++++[>++++++++++[>++++++++++[>++++++++++[>++++++++++[-]<-]<-]<-]<-]<-]<-]<-]++++++++++.";
+        brainfuck(test, sizeof(test));
+    } else {
+        // Easier to use unistd instead of stdio
+        int fd = open(argv[1], O_RDONLY);
+        if (fd < 0) {
+            printf("Could not open %s\n", argv[1]);
+            return 1;
+        }
+        struct stat st;
+        if (fstat(fd, &st) < 0) {
+            printf("Error statting %s\n", argv[1]);
+            close(fd);
+            return 1;
+        }
+        size_t len = st.st_size;
+        char *buf = (char *)malloc(len);
+        if (!buf) {
+            puts("Out of memory");
+            close(fd);
+            return 1;
+        }
+        if (read(fd, buf, len) < len) {
+            printf("Couldn't read %s\n", argv[1]);
+            close(fd);
+            free(buf);
+            return 1;
+        }
+        close(fd);
+        brainfuck(buf, len);
+    }
 }
 
